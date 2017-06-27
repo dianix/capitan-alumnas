c7d2bb44da2069fe066b538301383d479451b51f
dianix <dianavazquez.dcg@gmail.com>
2017-06-27 13:11:57 -0500

    creando funcion para publicar lista

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,12 +8,25 @@ var cargarPagina = function () {
     cargarLista();
 };
 
-var cargarLista = function(){
+var cargarLista = function () {
     //para llamar datos de API
-    $.getJSON(api.url,function(response){
-       console.log(response)
-    })
+    $.getJSON(api.url, function (alumnas) {
+        //console.log(response)
+        alumnas.forEach(formatoAlumna);
+    });
 };
+
+var formatoAlumna = function (alumna) {
+    // guardando datos necesarios
+    var numLista = alumna.id;
+    var nombreAlumna = alumna.name + " " + alumna.pLastName;
+    // creando elementos html por alumna
+    var $filaAlumna = $('<tr/>');
+    var $listaDato = $('<td/>').append(numLista);
+    var $nombreDato = $('<td/>').append(nombreAlumna);
+    
+    console.log(alumna)
+}
 
 
 $(document).ready(cargarPagina);
