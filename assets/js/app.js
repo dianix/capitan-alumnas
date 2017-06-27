866858c4e40054bf61c2577628e750085a49eef1
dianix <dianavazquez.dcg@gmail.com>
2017-06-27 16:36:34 -0500

    corregido conflicto de enlaces de api y gh-pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 var api = {
-    url: 'https://laboratoria.cuadra.co:9339/api/v1/students/'
+    url: 'http://laboratoria.cuadra.co:9339/api/v1/students/'
 };
 
 var $contenedorLista = $("#alumnList");
